c2ff5751bd3aba8c7addbb649289614c6e5f959f
Shreya Pai <105111079+PAI-SHREYA@users.noreply.github.com>
2024-06-23 15:36:18 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,47 +1,41 @@
-from mininet.log import setLogLevel, info
-from mininet.cli import CLI
-from mininet.node import Controller
-from mininet_wifi.net import Mininet_wifi
-from mininet_wifi.node import OVSKernelAP
-from mininet_wifi.link import wmediumd
-from mininet_wifi.wmediumdConnector import interference
+#!/usr/bin/python
 
-def create_custom_topology():
-    net = Mininet_wifi(controller=Controller, accessPoint=OVSKernelAP, link=wmediumd, wmediumd_mode=interference)
+
+import sys
+from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+from mininet.node import Controller
+def topology():
+    "Create a network."
+    net = Mininet_wifi(controller=Controller)
 
     info("*** Creating nodes\n")
-    # Create APs
-    ap1 = net.addAccessPoint('ap1', ssid="ssid_ap1", mode="g", channel="1", position='10,20,0')
-    ap2 = net.addAccessPoint('ap2', ssid="ssid_ap2", mode="g", channel="6", position='30,20,0')
-    
-    # Create stations
-    sta1 = net.addStation('sta1', ip='10.0.0.1/8', position='5,15,0')
-    sta2 = net.addStation('sta2', ip='10.0.0.2/8', position='15,15,0')
-    sta3 = net.addStation('sta3', ip='10.0.0.3/8', position='25,15,0')
-    sta4 = net.addStation('sta4', ip='10.0.0.4/8', position='35,15,0')
-    sta5 = net.addStation('sta5', ip='10.0.0.5/8', position='20,25,0')
-    sta6 = net.addStation('sta6', ip='10.0.0.6/8', position='30,25,0')
+    ap1 = net.addAccessPoint('ap1', ssid='ssid-ap1', mode='g', channel='1', position='10,30,0')
+    ap2 = net.addAccessPoint('ap2', ssid='ssid-ap2', mode='g', channel='6', position='50,30,0')
+    ap3 = net.addAccessPoint('ap3', ssid='ssid-ap3', mode='g', channel='11', position='90,30,0')
+    sta1 = net.addStation('sta1', ip='10.0.0.1', position='15,30,0')
+    sta2 = net.addStation('sta2', ip='10.0.0.2', position='20,40,0')
+    sta3 = net.addStation('sta3', ip='10.0.0.3', position='25,50,0')
+    sta4 = net.addStation('sta4', ip='10.0.0.4', position='30,60,0')
+    sta5 = net.addStation('sta5', ip='10.0.0.5', position='35,70,0')
+    sta6 = net.addStation('sta6', ip='10.0.0.6', position='40,80,0')
 
-    # Add controller
     c0 = net.addController('c0')
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
 
-    info("*** Associating and Creating Links\n")
+    info("*** Creating links\n")
     net.addLink(ap1, ap2)
-    
-    # Set propagation model parameters
-    net.propagationModel(model="logDistance", exp=4)
-
-    # Plot the network graphically
-    net.plotGraph(max_x=40, max_y=40)
+    net.addLink(ap2, ap3)
 
     info("*** Starting network\n")
     net.build()
     c0.start()
     ap1.start([c0])
     ap2.start([c0])
+    ap3.start([c0])
 
     info("*** Running CLI\n")
     CLI(net)
@@ -51,4 +45,4 @@ def create_custom_topology():
 
 if __name__ == '__main__':
     setLogLevel('info')
-    create_custom_topology()
+    topology()
