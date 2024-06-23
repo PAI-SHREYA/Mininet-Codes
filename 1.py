70ce840be97b186d4e12402aa324a081c057310e
Shreya Pai <105111079+PAI-SHREYA@users.noreply.github.com>
2024-06-23 13:56:39 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,56 +1,65 @@
-from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.node import OVSController
-from mininet.cli import CLI
-from mininet.link import TCLink
 from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.node import OVSKernelSwitch, Station
 
-class CustomTopo(Topo):
-    def build(self):
-        # Add switches
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
+def create_custom_topology():
+    "Create a custom Mininet-WiFi network."
 
-        # Add hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        h5 = self.addHost('h5')
-        h6 = self.addHost('h6')
+    # Create Mininet-WiFi network
+    net = Mininet_wifi(switch=OVSKernelSwitch, station=Station)
 
-        # Add links between switches to create redundancy
-        self.addLink(s1, s2)
-        self.addLink(s1, s3)
-        self.addLink(s2, s3)
+    # Define switches
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
 
-        # Add links between switches and hosts
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
-        self.addLink(h3, s2)
-        self.addLink(h4, s2)
-        self.addLink(h5, s3)
-        self.addLink(h6, s3)
+    # Define hosts
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
+    h4 = net.addHost('h4')
+    h5 = net.addHost('h5')
+    h6 = net.addHost('h6')
 
-def run():
-    topo = CustomTopo()
-    net = Mininet(topo=topo, controller=OVSController, link=TCLink)
-    net.start()
-    
-    # Test connectivity
-    print("Testing network connectivity")
+    # Connect hosts to switches
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s2)
+    net.addLink(h4, s2)
+    net.addLink(h5, s3)
+    net.addLink(h6, s3)
+
+    # Connect switches to create redundant paths
+    net.addLink(s1, s2)
+    net.addLink(s2, s3)
+    net.addLink(s3, s1)
+
+    return net
+
+def test_connectivity(net):
+    "Test connectivity between all hosts."
+
     net.pingAll()
 
-    # Print IP addresses of all hosts for verification
-    for host in net.hosts:
-        print(f'{host.name} has IP address {host.IP()}')
+def main():
+    "Main function to create and test the network."
 
-    # Start CLI
+    # Create custom Mininet-WiFi network
+    net = create_custom_topology()
+
+    # Start the network
+    net.start()
+
+    # Test connectivity
+    test_connectivity(net)
+
+    # Start the Mininet-WiFi CLI
     CLI(net)
 
+    # Stop the network
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
-    run()
+    main()
